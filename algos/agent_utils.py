3ef1e7af89cff031908d398c70d9fac90a670499
Mateo Pozo <mateopozoruiz02@hotmail.com>
2025-09-09 21:02:45 -0500

    feat: modify action sampling for stability

--- algos/agent_utils.py
+++ algos/agent_utils.py
@@ -8,8 +8,6 @@ class Base_Agent:
         
     def _do_random_actions(self, initial_steps: int) -> None:
         # Sample random actions depending on the type of action space
-        actions = np.random.uniform(low=np.array([self.env.params["sv_min"], self.env.params["v_min"]]), high=[self.env.params["sv_max"], self.env.params["v_max"]], size=(initial_steps, 2))
-
         for i in range(initial_steps):
             state, reward, terminated, info = self.env.reset(np.array([[0.7, 0, 1.37]]))  # Reset env and get initial state
             state = np.concatenate([
@@ -23,11 +21,13 @@ class Base_Agent:
             ])
             terminated, truncated = False, False  
             while not terminated and not truncated:  
-                action = np.array([actions[i]])  # While rollouts the target actor is used
-                #print(action)
+                action = np.random.uniform(
+                    low=np.array([self.env.params["sv_min"], self.env.params["v_min"]]),
+                    high=[self.env.params["sv_max"], self.env.params["v_max"]],
+                    size=(1, 2)
+                ).astype(np.float32)
+
                 next_state, reward, terminated, info = self.env.step(action)
-                # Print the robot position 
-                print("Robot position: x =", next_state['poses_x'][0], ", y =", next_state['poses_y'][0], ", theta =", next_state['poses_theta'][0])
                 next_state = np.concatenate([
                     next_state['scans'][0].flatten(),
                     np.array(next_state['poses_x']),
@@ -37,9 +37,8 @@ class Base_Agent:
                     np.array(next_state['linear_vels_y']),
                     np.array(next_state['ang_vels_z'])
                 ])
-                self.replay_buffer.add(state, action, reward, next_state, terminated, truncated)
+                self.replay_buffer.add(state, action[0], reward, next_state, terminated, truncated)
                 state = next_state
-                # print(state)
                 
     def get_device(self):
         return "cuda" if torch.cuda.is_available() else "cpu"
